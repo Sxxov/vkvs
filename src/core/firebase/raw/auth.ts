a98b9e3435fbe1caa6140d3ed491dbfb8464607b
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-01-04 19:16:17 +0800

    Unblock initial render while waiting for firestore

--- src/core/firebase/raw/auth.ts
+++ src/core/firebase/raw/auth.ts
@@ -1,5 +1,4 @@
-import { getAuth, signInAnonymously } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { app } from './app';
 
 export const auth = getAuth(app);
-await signInAnonymously(auth);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-	import { deleteDoc, doc, setDoc } from 'firebase/firestore';
+	import { signInAnonymously } from 'firebase/auth';
+	import {
+		type CollectionReference,
+		deleteDoc,
+		doc,
+		setDoc,
+	} from 'firebase/firestore';
 	import { ic_info, ic_search, ic_settings } from 'maic/two_tone';
 	import { quintOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
+	import { collectionStore } from 'sveltefire';
 	import BottomSheet, {
 		BottomSheetStates,
 	} from '../components/composable/BottomSheet.svelte';
+	import LineLoader from '../components/composable/loaders/LineLoader.svelte';
 	import Spacer from '../components/composable/Spacer.svelte';
 	import Svg from '../components/composable/Svg.svelte';
 	import BottomSheetFrame from '../components/composed/BottomSheetFrame.svelte';
@@ -14,8 +23,8 @@
 		toppingsW,
 	} from '../components/composed/nav/TopNav.svelte';
 	import Traverse from '../components/composed/Traverse.svelte';
+	import { auth } from '../core/firebase/raw/auth';
 	import { db } from '../core/firebase/raw/db';
-	import { defaultRef, defaultW } from '../core/firebase/stores/defaultW';
 	import { dropIn, dropOut } from '../core/transitioner/Transitioner';
 
 	$toppingsW = [
@@ -51,6 +60,26 @@
 			await setDoc(doc(db, defaultRef, docRef), nested, { merge: true });
 		};
 
+	const defaultRef = 'default';
+	let defaultW: ReturnType<typeof collectionStore> | undefined;
+
+	onMount(async () => {
+		await signInAnonymously(auth);
+
+		const defW = collectionStore<{
+			ref: CollectionReference;
+			id: string;
+			[k: string]: unknown;
+		}>(db, defaultRef);
+
+		const unsubscribe = defW.subscribe(() => {
+			setTimeout(() => {
+				unsubscribe();
+			});
+			defaultW = defW;
+		});
+	});
+
 	let aboutState = BottomSheetStates.IDLE;
 </script>
 
@@ -59,94 +88,103 @@
 	class="component"
 >
 	<div class="editor">
-		<Traverse
-			object={Object.fromEntries(
-				$defaultW.map((collection) => [
-					collection.id,
-					Object.fromEntries(
-						Object.entries(collection).filter(
-							([k]) => k !== 'id' && k !== 'ref',
+		{#if defaultW}
+			<Traverse
+				object={Object.fromEntries(
+					$defaultW.map((collection) => [
+						collection.id,
+						Object.fromEntries(
+							Object.entries(collection).filter(
+								([k]) => k !== 'id' && k !== 'ref',
+							),
 						),
-					),
-				]),
-			)}
-			let:key
-			let:value
-			let:history
-		>
-			{@const update = generateUpdateDefaultDoc(history)}
-			<div
-				slot="before-enter"
-				let:setShouldDoNextEnter
+					]),
+				)}
+				let:key
+				let:value
+				let:history
 			>
+				{@const update = generateUpdateDefaultDoc(history)}
 				<div
-					class="indent object"
-					style="
+					slot="before-enter"
+					let:setShouldDoNextEnter
+				>
+					<div
+						class="indent object"
+						style="
 						--indent: {history.length};
 					"
+						in:dropIn
+						out:dropOut={{ easing: quintOut, duration: 100 }}
+					>
+						<KeyValueEdit
+							{key}
+							{value}
+							isDeleteable={history.length <= 0}
+							on:delete={({ detail: { key } }) => {
+								void deleteDoc(
+									doc(
+										db,
+										defaultRef,
+										...history,
+										String(key),
+									),
+								);
+							}}
+							on:collapse={() => {
+								setShouldDoNextEnter(false);
+							}}
+							on:expand={() => {
+								setShouldDoNextEnter(true);
+							}}
+						/>
+					</div>
+				</div>
+				<div
+					class="indent primitive"
+					style="
+					--indent: {history.length};
+				"
 					in:dropIn
 					out:dropOut={{ easing: quintOut, duration: 100 }}
 				>
 					<KeyValueEdit
 						{key}
 						{value}
-						isDeleteable={history.length <= 0}
-						on:delete={({ detail: { key } }) => {
-							void deleteDoc(
-								doc(db, defaultRef, ...history, String(key)),
-							);
-						}}
-						on:collapse={() => {
-							setShouldDoNextEnter(false);
-						}}
-						on:expand={() => {
-							setShouldDoNextEnter(true);
+						isDeleteable={false}
+						on:submit={({ detail: { key, value } }) => {
+							update(key, value);
 						}}
 					/>
 				</div>
-			</div>
-			<div
-				class="indent primitive"
-				style="
-					--indent: {history.length};
-				"
-				in:dropIn
-				out:dropOut={{ easing: quintOut, duration: 100 }}
-			>
-				<KeyValueEdit
-					{key}
-					{value}
-					isDeleteable={false}
-					on:submit={({ detail: { key, value } }) => {
-						update(key, value);
-					}}
-				/>
-			</div>
-			<div slot="after-enter">
-				<Spacer height={2} />
-			</div>
-		</Traverse>
-		<p
-			class="new indent"
-			style="--indent: 0"
-			on:keydown={({ currentTarget, key }) =>
-				void (key === 'Enter' && currentTarget.click())}
-			on:click={() => {
-				void setDoc(
-					doc(db, defaultRef, String($defaultW.length)),
-					{
-						name: 'Untitled',
-						transform: {
-							position: { x: 0, y: 0, z: 0 },
-							rotation: { x: 0, y: 0, z: 0 },
+				<div slot="after-enter">
+					<Spacer height={2} />
+				</div>
+			</Traverse>
+			<p
+				class="new indent"
+				style="--indent: 0"
+				on:keydown={({ currentTarget, key }) =>
+					void (key === 'Enter' && currentTarget.click())}
+				on:click={() => {
+					void setDoc(
+						doc(db, defaultRef, String($defaultW.length)),
+						{
+							name: 'Untitled',
+							transform: {
+								position: { x: 0, y: 0, z: 0 },
+								rotation: { x: 0, y: 0, z: 0 },
+							},
 						},
-					},
-					{ merge: true },
-				);
-			}}
-		>
-			Add new item...
-		</p>
+						{ merge: true },
+					);
+				}}
+			>
+				Add new item...
+			</p>
+		{:else}
+			<LineLoader />
+		{/if}
 	</div>
 	<BottomSheet bind:state={aboutState}>
 		<BottomSheetFrame>
